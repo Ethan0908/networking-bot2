0d773d13b08e833396b37a40d05eaa4304272e8d
Ethan0908 <44311213+Ethan0908@users.noreply.github.com>
2025-09-06 19:55:23 -0700

    Add Next.js and TypeScript configuration

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import path from "path";
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  },
+};
+
+export default nextConfig;
