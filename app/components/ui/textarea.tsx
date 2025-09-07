e2ab175f38c77b41f01c072ab9accea3bd7e4784
Ethan0908 <44311213+Ethan0908@users.noreply.github.com>
2025-09-06 22:02:09 -0700

    Add basic rolodex UI with n8n webhook integration

---
+++ app/components/ui/textarea.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+export function Textarea({ className = '', ...props }: TextareaProps) {
+  return <textarea className={`border rounded px-2 py-1 text-sm ${className}`} {...props} />;
+}
